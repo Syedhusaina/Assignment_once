60478abd946261af9fca7723d9819f4e7869b827
Syedhusaina <120296651+Syedhusaina@users.noreply.github.com>
2023-01-03 13:01:57 +0530

    Inheritance

---
+++ inheritoncenew/src/com/inheritoncenew/Mentor.java
@@ -0,0 +1,15 @@
+package com.inheritoncenew;
+
+public class Mentor extends InheritA {
+		 String mainSubject = "Core...Java";  
+		public static void main(String args[])
+		{   
+			 Mentor obj = new Mentor();    
+			    
+			System.out.println(obj.Source);    
+			System.out.println(obj.designation);   
+			System.out.println(obj.mainSubject);  
+	    obj.does();  
+	    }
+} 
+
