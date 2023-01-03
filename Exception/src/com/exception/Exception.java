fd61d0d1828311765997987119eba83946a7cf6f
Syedhusaina <120296651+Syedhusaina@users.noreply.github.com>
2023-01-03 16:37:31 +0530

    Eception handling try cache finally

--- Exception/src/com/exception/Exception.java
+++ Exception/src/com/exception/Exception.java
@@ -3,7 +3,7 @@ package com.exception;
 public class Exception 
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
 		try
